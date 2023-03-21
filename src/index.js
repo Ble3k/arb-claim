989fe16a51c35f3d84e607b7afe096c2487577e6
rfateev <rufateev@gmail.com>
2023-03-21 17:32:59 +0300

    comments

--- src/index.js
+++ src/index.js
@@ -30,62 +30,62 @@ let blockNumberProcessing = null;
 
 ethProvider.on("block", async (blockNumber) => {
   console.log(blockNumber, "-----Ethereum------");
-  if (blockNumberProcessing !== blockNumber) {
-    inspect(`Processing block: #${blockNumber}`);
-    blockNumberProcessing = blockNumber;
-
-    if (blockNumber === +CLAIM_START_ON_BLOCK) {
-      const currentNonce = await doRequestSafeRepeat({
-        request: async () => await wallet.getTransactionCount(),
-        onFailedMessaged: `Failed to get transaction count on block #${blockNumber}`,
-        waitTimeMS: WAIT_PER_REQUEST_TIME,
-      });
-      const claimTx = await doRequestSafeRepeat({
-        request: async () =>
-          await arbDistributor.connect(wallet).claim({
-            gasLimit: "0x4C4B40",
-            gasPrice: "0x3B9ACA00",
-            nonce: currentNonce,
-          }),
-        onFailedMessaged: `Failed to call claim method on block #${blockNumber}`,
-        waitTimeMS: WAIT_PER_REQUEST_TIME,
-      });
-
-      inspect(`Claim was called successful! Tx hash: ${claimTx.hash}`);
-
-      let balance;
-      let balanceFormatted;
-      do {
-        await wait(BALANCE_CHECK_REPEAT_TIME);
-
-        try {
-          balance = await arbToken.balanceOf(wallet.address);
-          balanceFormatted = +ethers.utils.formatUnits(balance, ARB_TOKEN_DECIMALS);
-          inspect(`Current balance for wallet ${wallet.address} - ${balanceFormatted} ARB`);
-        } catch (e) {
-          inspect(`Failed to get ARB balance of wallet ${wallet.address} on block ${blockNumber}`);
-          inspect(e);
-        }
-      } while (balanceFormatted === 0);
-
-      for (let i = 0; i < SEND_TO.length; i++) {
-        const { name, address, amount } = SEND_TO[i];
-
-        const amountBN = ethers.utils.parseUnits(amount, ARB_TOKEN_DECIMALS);
-        const transferTx = await doRequestSafeRepeat({
-          request: async () =>
-            await arbToken.connect(wallet).transfer(address, amountBN, {
-              gasLimit: "0x4C4B40",
-              gasPrice: "0x3B9ACA00",
-              nonce: currentNonce + 1 + i,
-            }),
-          onFailedMessaged: `Failed to call transfer method on block #${blockNumber}`,
-          waitTimeMS: WAIT_PER_REQUEST_TIME,
-        });
-        inspect(`Transfer to ${address} (${name}) was called successful! Tx hash: ${transferTx.hash}`);
-      }
-    }
-  }
+  // if (blockNumberProcessing !== blockNumber) {
+  //   inspect(`Processing block: #${blockNumber}`);
+  //   blockNumberProcessing = blockNumber;
+  //
+  //   if (blockNumber === +CLAIM_START_ON_BLOCK) {
+  //     const currentNonce = await doRequestSafeRepeat({
+  //       request: async () => await wallet.getTransactionCount(),
+  //       onFailedMessaged: `Failed to get transaction count on block #${blockNumber}`,
+  //       waitTimeMS: WAIT_PER_REQUEST_TIME,
+  //     });
+  //     const claimTx = await doRequestSafeRepeat({
+  //       request: async () =>
+  //         await arbDistributor.connect(wallet).claim({
+  //           gasLimit: "0x4C4B40",
+  //           gasPrice: "0x3B9ACA00",
+  //           nonce: currentNonce,
+  //         }),
+  //       onFailedMessaged: `Failed to call claim method on block #${blockNumber}`,
+  //       waitTimeMS: WAIT_PER_REQUEST_TIME,
+  //     });
+  //
+  //     inspect(`Claim was called successful! Tx hash: ${claimTx.hash}`);
+  //
+  //     let balance;
+  //     let balanceFormatted;
+  //     do {
+  //       await wait(BALANCE_CHECK_REPEAT_TIME);
+  //
+  //       try {
+  //         balance = await arbToken.balanceOf(wallet.address);
+  //         balanceFormatted = +ethers.utils.formatUnits(balance, ARB_TOKEN_DECIMALS);
+  //         inspect(`Current balance for wallet ${wallet.address} - ${balanceFormatted} ARB`);
+  //       } catch (e) {
+  //         inspect(`Failed to get ARB balance of wallet ${wallet.address} on block ${blockNumber}`);
+  //         inspect(e);
+  //       }
+  //     } while (balanceFormatted === 0);
+  //
+  //     for (let i = 0; i < SEND_TO.length; i++) {
+  //       const { name, address, amount } = SEND_TO[i];
+  //
+  //       const amountBN = ethers.utils.parseUnits(amount, ARB_TOKEN_DECIMALS);
+  //       const transferTx = await doRequestSafeRepeat({
+  //         request: async () =>
+  //           await arbToken.connect(wallet).transfer(address, amountBN, {
+  //             gasLimit: "0x4C4B40",
+  //             gasPrice: "0x3B9ACA00",
+  //             nonce: currentNonce + 1 + i,
+  //           }),
+  //         onFailedMessaged: `Failed to call transfer method on block #${blockNumber}`,
+  //         waitTimeMS: WAIT_PER_REQUEST_TIME,
+  //       });
+  //       inspect(`Transfer to ${address} (${name}) was called successful! Tx hash: ${transferTx.hash}`);
+  //     }
+  //   }
+  // }
 });
 
 arbitrumProvider.on("block", async (blockNumber) => {
